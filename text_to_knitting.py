953d902c18b6bc3dba5a6acdec29826e38716d67
Alison <aspencer03@gmail.c>
2024-05-03 10:19:13 -0500

    update blocks per line option, add test to script

--- text_to_knitting.py
+++ text_to_knitting.py
@@ -1,6 +1,10 @@
 import re
 from typing import List
+# from itertools import batched
 import argparse
+
+test_text = 'test helloworldsofwater how are you doing today 2024-05-03?'
+expected_test_output = 'p4 k1 p5 \np7 k1 p2 \np4 k1 p3 k1 p1 \np2 k1 p3 k1 p3 \np2 k1 p5 k1 p1 \np1 k1 p2 k2 p2 k1 p1 \np3 k2 p2 k2 p1 \np4 k2 p2 k2 \np3 k2 '
 
 def text_to_stitches(text: str, punc_spaces: int=2, zero_as_punctuation: bool=True):
     stitches = ''
@@ -48,10 +52,6 @@ def fix_long_repeats(string: str,
         
     return string
 
-
-def break_sequence(sequence: str, grouping_size: int=10): 
-    return [sequence[i:i+grouping_size] for i in range(0, len(sequence), grouping_size)]
-
 def consolidate_sequence_into_pattern(sequence: str):
     consolidated = ''
     current = sequence[0]
@@ -76,8 +76,31 @@ def consolidate_sequences(sequences: List[str]):
         
     return '\n'.join(pattern)
 
+def break_sequence(sequence: str, group_size): 
+    return [sequence[i:i+group_size] for i in range(0, len(sequence), group_size)]
+
+# itertools version; only avalaible in python 3.10 or later 
+# def make_readable_pattern(sequence: str, block_size: int=10, blocks_in_line: int=3): 
+
+#     blocks = [consolidate_sequence_into_pattern(''.join(b)) for b in batched(sequence, block_size)]
+#     lines = ['\t'.join((l)) for l in batched(blocks, blocks_in_line)]
+    
+#     return '\n'.join(lines)
+
+def make_readable_pattern(sequence: str, block_size: int=10, blocks_in_line: int=1): 
+
+    blocks = [consolidate_sequence_into_pattern(b) for b in break_sequence(sequence, block_size)]
+
+    lines = []
+    while len(blocks) > 0:
+        lines.append('\t'.join(blocks[:blocks_in_line]))
+        blocks = blocks[blocks_in_line:]
+    
+    return '\n'.join(lines)
+
 def text_to_knitting(text: str, 
-                     stitches_per_line: int=10, 
+                     stitches_per_block: int=10, 
+                     blocks_per_line: int=1,
                      max_purl_run_len: int=12, 
                      max_knit_run_len: int=2, 
                      purl_run_spacer_char: str='k', 
@@ -93,11 +116,13 @@ def text_to_knitting(text: str,
     stitches = fix_long_repeats(stitches, 
                                 max_purl_run_len=max_purl_run_len, 
                                 max_knit_run_len=max_knit_run_len,
-                                purl_run_spacer_char=purl_run_spacer_char)
-    
-    stitch_groups = break_sequence(stitches, grouping_size=stitches_per_line)
-    
-    pattern = consolidate_sequences(stitch_groups)
+                                purl_run_spacer_char=purl_run_spacer_char
+                                )
+        
+    pattern = make_readable_pattern(stitches, 
+                                    block_size=stitches_per_block, 
+                                    blocks_in_line=blocks_per_line
+                                    )
     
     return pattern
 
@@ -105,14 +130,13 @@ def text_to_knitting(text: str,
 def divide_original_text(text: str, grouping_size: int=10):
     return '\n'.join([text[i:i+grouping_size] for i in range(0, len(text), grouping_size)]).strip()
 
-test_text = 'test helloworldsofwater how are you doing today 2024-05-03?'
-
 
 if __name__ == '__main__':
     
     argparser = argparse.ArgumentParser(description='Convert text to knitting pattern')
     argparser.add_argument('--text', type=str, default=test_text, help='Text to convert to knitting pattern')
-    argparser.add_argument('--stitches_per_line', type=int, default=10, help='Number of stitches per line')
+    argparser.add_argument('--stitches_per_block', type=int, default=10, help='Number of stitches per group in final pattern')
+    argparser.add_argument('--blocks_per_line', type=int, default=1, help='Number of stitch blocks per line')    
     argparser.add_argument('--max_purl_run_len', type=int, default=12, help='Maximum length of purl run before inserting spacer')
     argparser.add_argument('--max_knit_run_len', type=int, default=2, help='Maximum length of knit run before shortening')
     argparser.add_argument('--purl_run_spacer_char', type=str, default='k', help='Character to insert between purl runs')
@@ -120,6 +144,8 @@ if __name__ == '__main__':
     argparser.add_argument('--treat_zero_as_punctuation', type=bool, default=True, help='Treat 0 as punctuation')
     
     pattern = text_to_knitting(**vars(argparser.parse_args()))    
-    print(pattern)
         
+    assert pattern == expected_test_output, 'Test failed'
     
+    print(pattern)
+
