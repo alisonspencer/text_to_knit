2976d314c10aed60bbfda1a23e3cce4d6416a38e
Alison <aspencer03@gmail.c>
2024-05-04 09:34:06 -0500

    update

---
+++ lyrics.py
@@ -0,0 +1,114 @@
+texts = ['''I never felt magic crazy as this
+I never saw moons, knew the meaning of the sea
+I never held emotion in the palm of my hand
+Or felt sweet breezes in the top of a tree
+But now you're here
+Brighten my northern sky
+I've been a long time that I'm waiting
+Been a long that I'm blown
+I've been a long time that I've wandered
+Through the people I have known
+Oh, if you would and you could
+Straighten my new mind's eye
+Would you love me for my money?
+Would you love me for my head?
+Would you love me through the winter?
+Would you love me 'til I'm dead?
+Oh, if you would and you could
+Come blow your horn on high
+I never felt magic crazy as this
+I never saw moons, knew the meaning of the sea
+I never held emotion in the palm of my hand
+Or felt sweet breezes in the top of a tree
+But now you're here
+Brighten my northern sky
+'''
+, '''
+Pray God you can cope
+I stand outside this woman's work
+This woman's world
+Ooh, it's hard on the man
+Now his part is over
+Now starts the craft of the father
+I know you have a little life in you yet
+I know you have a lot of strength left
+I know you have a little life in you yet
+I know you have a lot of strength left
+I should be crying, but I just can't let it show
+I should be hoping, but I can't stop thinking
+Of all the things I should've said
+That I never said
+All the things we should've done
+That we never did
+All the things I should've given
+But I didn't
+Oh, darling, make it go
+Make it go away
+Give me these moments back
+Give them back to me
+Give me that little kiss
+Give me your hand
+I know you have a little life in you yet
+I know you have a lot of strength left
+I know you have a little life in you yet
+I know you have a lot of strength left
+I should be crying, but I just can't let it show
+I should be hoping, but I can't stop thinking
+Of all the things we should've said
+That were never said
+All the things we should've done
+That we never did
+All the things that you needed from me
+All the things that you wanted for me
+All the things that I should've given but I didn't
+Oh, darling, make it go away
+Just make it go away now
+'''
+, '''
+My life has been a tapestry
+Of rich and royal hue
+An everlasting vision
+Of the ever-changing view
+A wond'rous woven magic
+In bits of blue and gold
+A tapestry to feel and see
+Impossible to hold
+Once amid the soft silver
+Sadness in the sky
+There came a man of fortune
+A drifter passing by
+He wore a torn and tattered cloth
+Around his leathered hide
+And a coat of many colors
+Yellow, green, on either side
+He moved with some uncertainty
+As if he didn't know
+Just what he was there for
+Or where he ought to go
+Once he reached for something
+Golden hanging from a tree
+And his hand came down empty
+Soon within my tapestry
+Along the rutted road
+He sat down on a river rock
+And turned into a toad
+It seemed that he had fallen
+Into someone's wicked spell
+And I wept to see him suffer
+Though I didn't know him well
+As I watched in sorrow
+There suddenly appeared
+A figure gray and ghostly
+Beneath a flowing beard
+In times of deepest darkness
+I've seen him dressed in black
+Now my tapestry's unraveling
+He's come to take me back
+He's come to take me back
+'''
+]
+
+spacer_sequence = 'a 1 2 3 4 '
+# spacer_sequence = '  x x x x x x  '
+
+text = spacer_sequence.join(texts)
